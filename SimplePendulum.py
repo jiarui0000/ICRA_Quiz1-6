49de9523bb27f95ddcd08be9e2378066a6ac374d
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-09-30 17:29:23 +0800

    Update SimplePendulum.py
    separate tester from the class

--- SimplePendulum.py
+++ SimplePendulum.py
@@ -86,10 +86,3 @@ class SimplePendulum:
         self.getCurrentSituation()
         return
 
-
-b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithX(-10)
-b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithY(48.988)
-b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithTime(4.261)
