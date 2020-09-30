f60a3d2554f1711b30b319c25b030ae80704f2d7
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-09-30 17:29:30 +0800

    Create SimplePendulumTesater.py
    separate tester from the class

---
+++ SimplePendulumTesater.py
@@ -0,0 +1,8 @@
+from SimplePendulum import SimplePendulum
+
+b = SimplePendulum(40, 30, 200000)
+b.getVelocityWithX(-10)
+b = SimplePendulum(40, 30, 200000)
+b.getVelocityWithY(48.988)
+b = SimplePendulum(40, 30, 200000)
+b.getVelocityWithTime(4.261)
