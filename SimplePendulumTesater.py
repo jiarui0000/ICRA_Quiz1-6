b8efe3ace3bd1fee144d6f79b733a6e8fe6684fd
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-09-30 17:57:17 +0800

    Update SimplePendulumTesater.py
    change some function name

--- SimplePendulumTesater.py
+++ SimplePendulumTesater.py
@@ -1,8 +1,6 @@
 from SimplePendulum import SimplePendulum
 
 b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithX(-10)
-b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithY(48.988)
-b = SimplePendulum(40, 30, 200000)
-b.getVelocityWithTime(4.261)
+b.getSituationWithX(-10)
+b.getSituationWithY(48.988)
+b.getSituationWithTime(4.261)
