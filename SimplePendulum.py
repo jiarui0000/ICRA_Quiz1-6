d837d3b1ea32e55f7651f226d9e1bb6acd9a5126
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-09-30 18:45:46 +0800

    update 2.0
    All datas are output as function return values.

--- SimplePendulum.py
+++ SimplePendulum.py
@@ -15,86 +15,108 @@ class SimplePendulum:
     __runtime = 0.0  # (s)
 
     def __init__(self, startPoint_x, startPoint_y, frequency):
-        self.runtime = 0.0
-        self.startPoint_x = startPoint_x
-        self.startPoint_y = startPoint_y
-        self.frequency = frequency
-        self.currentPoint_x = startPoint_x
-        self.currentPoint_y = startPoint_y
-        self.theta = math.atan(abs(self.__currentPoint_x / self.__currentPoint_y))
-        self.ropeLength = math.sqrt(startPoint_x * startPoint_x + startPoint_y * startPoint_y)
-        self.velocity = 0.0
-        self.acc = 0.0
+        self.__runtime = 0.0
+        self.__startPoint_x = startPoint_x
+        self.__startPoint_y = startPoint_y
+        self.__frequency = frequency
+        self.__currentPoint_x = startPoint_x
+        self.__currentPoint_y = startPoint_y
+        if self.__currentPoint_y != 0:
+            self.__theta = math.atan(abs(self.__currentPoint_x / self.__currentPoint_y))
+        else:
+            self.__theta = math.pi / 2
+        self.__ropeLength = math.sqrt(startPoint_x * startPoint_x + startPoint_y * startPoint_y)
+        self.__velocity = 0.0
+        self.__acc = 0.0
+        return
+
+    def setFrenquncy(self, frequency):
+        self.__frequency = frequency
         return
 
     def reset(self):
-        self.runtime = 0.0
-        self.currentPoint_x = startPoint_x
-        self.currentPoint_y = startPoint_y
-        self.theta = math.atan(abs(self.currentPoint_x / self.currentPoint_y))
-        self.velocity = 0.0
-        self.acc = 0.0
+        self.__runtime = 0.0
+        self.__currentPoint_x = self.__startPoint_x
+        self.__currentPoint_y = self.__startPoint_y
+        self.__theta = math.atan(abs(self.__currentPoint_x / self.__currentPoint_y))
+        self.__velocity = 0.0
+        self.__acc = 0.0
         return
 
     def __accelerationCalculate(self):
-        self.acc = self.accOfGravity * math.sin(self.theta)
+        self.__acc = self.__accOfGravity * math.sin(self.__theta)
         return
 
     def __velocityCalculate(self):
-        self.accelerationCalculate()
-        self.velocity += self.acc / self.frequency
+        self.__accelerationCalculate()
+        self.__velocity += self.__acc / self.__frequency
         #print(self.velocity)
         return
 
     def __positionCalculate(self):
-        self.velocityCalculate()
-        self.currentPoint_x -= self.velocity * math.cos(self.theta) / self.frequency
-        self.currentPoint_y += self.velocity * math.sin(self.theta) / self.frequency
-        self.theta = math.atan(self.currentPoint_x / self.currentPoint_y)
+        self.__velocityCalculate()
+        self.__currentPoint_x -= self.__velocity * math.cos(self.__theta) / self.__frequency
+        self.__currentPoint_y += self.__velocity * math.sin(self.__theta) / self.__frequency
+        self.__theta = math.atan(self.__currentPoint_x / self.__currentPoint_y)
         # self.getCurrentSituation()
         self.__errorCorrection()
         return
 
-    def __moveThroughTime(self):
-        self.runtime += 1 / self.frequency
-        self.positionCalculate()
+    def moveThroughTime(self):
+        self.__runtime += 1 / self.__frequency
+        self.__positionCalculate()
         return
 
     def __errorCorrection(self):
-        error_length = math.sqrt(self.currentPoint_x * self.currentPoint_x + self.currentPoint_y * self.currentPoint_y)
-        self.currentPoint_x = self.currentPoint_x * self.ropeLength / error_length
-        self.currentPoint_y = self.currentPoint_y * self.ropeLength / error_length
+        error_length = math.sqrt(self.__currentPoint_x * self.__currentPoint_x + self.__currentPoint_y * self.__currentPoint_y)
+        self.__currentPoint_x = self.__currentPoint_x * self.__ropeLength / error_length
+        self.__currentPoint_y = self.__currentPoint_y * self.__ropeLength / error_length
         return
 
+    def getCurrentX(self):
+        x = self.__currentPoint_x
+        return x
+
+    def getCurrentY(self):
+        y = self.__currentPoint_y
+        return y
+
     def getCurrentSituation(self):
-        print('The line velocity is %.4f ' % self.velocity)
-        print('The time cost is %.4f ' % self.runtime)
-        print("x = %.7f" % self.currentPoint_x)
-        print("y = %.7f" % self.currentPoint_y)
-        print("l = %.7f" % math.sqrt(self.currentPoint_x * self.currentPoint_x + self.currentPoint_y * self.currentPoint_y))
+        v = self.__velocity
+        r = self.__runtime
+        x = self.__currentPoint_x
+        y = self.__currentPoint_y
+        return v, r, x, y
+
+    def printCurrentSituation(self):
+        print('The line velocity is %.4f ' % self.__velocity)
+        print('The time cost is %.4f ' % self.__runtime)
+        print("x = %.7f" % self.__currentPoint_x)
+        print("y = %.7f" % self.__currentPoint_y)
+        # print("l = %.7f" % math.sqrt(self.currentPoint_x * self.currentPoint_x + self.currentPoint_y * self.currentPoint_y))
         return
 
     def getSituationWithX(self, target_x):
         self.reset()
-        while (abs(self.currentPoint_x - target_x) > 0.001):
-            self.__moveThroughTime()
+        while abs(self.__currentPoint_x - target_x) > 0.0001:
+            self.moveThroughTime()
         print("Get Velocity With X")
-        self.getCurrentSituation()
-        return
+        t = self.getCurrentSituation()
+        return t
 
     def getSituationWithY(self, target_y):
         self.reset()
-        while (abs(self.currentPoint_y - target_y) > 0.001):
-            self.__moveThroughTime()
+        while abs(self.__currentPoint_y - target_y) > 0.0001:
+            self.moveThroughTime()
         print("Get Velocity With Y")
-        self.getCurrentSituation()
-        return
+        t = self.getCurrentSituation()
+        return t
 
     def getSituationWithTime(self, target_time):
         self.reset()
-        while (abs(self.runtime - target_time) > 0.000001):
-            self.__moveThroughTime()
+        while abs(self.__runtime - target_time) > 0.000001:
+            self.moveThroughTime()
         print("Get Velocity With Time")
-        self.getCurrentSituation()
-        return
+        t = self.getCurrentSituation()
+        return t
 
