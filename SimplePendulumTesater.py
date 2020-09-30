fca2356d67bf8668e59fa549c448a0cff9803860
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-09-30 18:46:21 +0800

    tester 2.0
    change data output method

--- SimplePendulumTesater.py
+++ SimplePendulumTesater.py
@@ -1,6 +1,7 @@
 from SimplePendulum import SimplePendulum
 
 b = SimplePendulum(40, 30, 200000)
-b.getSituationWithX(-10)
-b.getSituationWithY(48.988)
-b.getSituationWithTime(4.261)
+print(b.getSituationWithX(-10))
+print(b.getSituationWithY(48.988))
+print(b.getSituationWithTime(4.2615))
+print("x= ", b.getCurrentX(), ", y= ", b.getCurrentY())
